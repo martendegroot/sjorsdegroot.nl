dbe83f62b0634529c1c339c17c6b031a454ff666
Marten de Groot <mdegroot@xebia.com>
2024-10-29 21:23:10 +0100

    Homepage profielfoto toegevoegd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
+import profilePic from "public/profile.webp";
+
 export default function Home() {
   return (
-    <main className="col-start-2 col-span-8 flex flex-col gap-6 grow justify-center text-3xl mb-[15vh]">
+    <main className="col-start-2 col-span-10 flex flex-col gap-6 grow justify-center text-3xl mb-[15vh]">
       <p>Ontdekken wie je bent</p>
       <p>en wat je werkelijk wil</p>
+      <Image
+        src={profilePic}
+        alt="Foto van Sjors"
+        className="absolute bottom-0 h-[90vh] w-auto -z-10 left-[55vw]"
+      />
     </main>
   );
 }

8cd68a08564c4d279d727e900777973b7b8483a4
Marten de Groot <mdegroot@xebia.com>
2024-11-17 22:27:42 +0100

    Fixed favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,29 @@ const quicksand = Quicksand({
 export const metadata: Metadata = {
   title: "Sjors de Groot",
   description: "Transpersoonlijke coaching",
+  icons: [
+    {
+      rel: "icon",
+      url: "/favicon.ico",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon/favicon-16x16.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicon/apple-touch-icon.png",
+    },
+  ],
 };
 
 export default function RootLayout({
